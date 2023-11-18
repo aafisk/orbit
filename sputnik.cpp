10d0b82aa9df87af601ffaca5c9dffb9360252f1
aafisk <axelfisk12@gmail.com>
2023-11-18 15:48:43 -0700

    cout's removed

--- sputnik.cpp
+++ sputnik.cpp
@@ -4,22 +4,12 @@ using namespace std;
 
 void Sputnik::applyPhysics(PhysicsManager &physicsManager)
 {
-	cout << "position: " << position << endl;
-	cout << "velocity: " << velocity.getDx() << endl;
-	cout << "velocity: " << velocity.getDy() << endl;
-	cout << "acceleration: " << acceleration.getDdx() << endl;
-	cout << "acceleration: " << acceleration.getDdy() << endl;
 	position.setMetersX(physicsManager.calculateDistance(position.getMetersX(), velocity.getDx(), acceleration.getDdx()));
 	position.setMetersY(physicsManager.calculateDistance(position.getMetersY(), velocity.getDy(), acceleration.getDdy()));
 
 	velocity.setDx(physicsManager.calculateVelocity(velocity.getDx(), acceleration.getDdx()));
 	velocity.setDy(physicsManager.calculateVelocity(velocity.getDy(), acceleration.getDdy()));
 	
-	cout << "position: " << position << endl;
-	cout << "velocity: " << velocity.getDx() << endl;
-	cout << "velocity: " << velocity.getDy() << endl;
-	cout << "acceleration: " << acceleration.getDdx() << endl;
-	cout << "acceleration: " << acceleration.getDdy() << endl;
 }
 
 
