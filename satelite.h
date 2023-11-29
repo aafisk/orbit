8daef6cb75d5b6e711146fa6831a61d09cbccc69
aafisk <axelfisk12@gmail.com>
2023-11-29 12:44:28 -0700

    satelite and orbiter classes merged

--- satelite.h
+++ satelite.h
@@ -1,15 +1,38 @@
 #pragma once
 
-#include "orbiter.h"
+#include "position.h"
+#include "velocity.h"
+#include "acceleration.h"
+#include "physics.h"
+#include "uiDraw.h"
 
-class Satelite : Orbiter
+class Orbiter
 {
 public:
-	Satelite() : Orbiter() { type = "none"; }
-
+	Orbiter()
+	{
+		position = Position();
+		velocity = Velocity();
+		acceleration = Acceleration();
+		angle = 0.0;
+		isAlive = true;
+	}
+	Position getPosition() const { return position; }
+	Velocity getVelocity() const { return velocity; }
+	Acceleration getAcceleration() const { return acceleration; }
+	double getAngle() const { return angle; }
 	string getType() { return type; }
+	bool getIsAlive() const { return isAlive; }
+	void setToDead() { isAlive = false; }
+	virtual void applyPhysics(const PhysicsManager& physics) = 0;
+	virtual void draw(const ogstream& gout) const = 0;
 
 private:
+	Position position;
+	Velocity velocity;
+	Acceleration acceleration;
+	double angle;
 	string type;
+	bool isAlive;
 };
 
